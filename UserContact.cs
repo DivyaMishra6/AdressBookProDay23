e63196144ecb2b1ab03b5184d88f03f3ba9275e5
DivyaMishra6 <divyamishra599@gmail.com>
2023-04-14 14:53:43 +0530

    Display all list

---
+++ UserContact.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day23_AddressBook
+{
+    public class UserContact
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string mobileNumber { get; set; }
+        public string address { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string zip { get; set; }
+        public string email { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name: {firstName} {lastName}\nMobile Number: {mobileNumber}\nAddress: {address}\nCity: {city}\nState: {state}\nZip: {zip}\nEmail: {email}\n";
+        }
+    }
+}
