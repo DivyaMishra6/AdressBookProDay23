5364663fc59d95663109b3ba2b5b245eabcdd09b
DivyaMishra6 <divyamishra599@gmail.com>
2023-04-14 14:35:30 +0530

    User Main Program

---
+++ AddressBookMainClass.cs
@@ -0,0 +1,12 @@
+﻿namespace Day23_AddressBook
+{
+    class AddressBookMainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("!!!!!!Welcome to Updated Adress Book Program!!!!!!");
+            Console.WriteLine(" ");
+        }
+    }
+}
